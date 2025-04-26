1cf43a57fbdaf67dffca7164dfbe08b44c4f77ca
superkidsyuvan <meetnandhini@outlook.com>
2025-04-26 16:04:57 -0500

    #first commit 😇

---
+++ README.md
@@ -0,0 +1,11 @@
+#shark tank!!!
+
+dont use the `cargo run` func in terminal to setup localhost 
+
+visit 127.0.0.1 in the browser 😱
+
+shark tank exists
+
+will be updated
+
+no i will not comment my code stop askings
---
+++ main.rs
@@ -0,0 +1,34 @@
+use std::{io::{BufRead, Write}, path::Path};
+fn main() {
+    let listener: std::net::TcpListener = std::net::TcpListener::bind("127.0.0.1:9999").unwrap();
+    for mut stream in listener.incoming().flatten() {
+        let mut rdr: std::io::BufReader<&mut std::net::TcpStream> = std::io::BufReader::new(&mut stream);
+        let mut l: String = String::new();
+        rdr.read_line(&mut l).unwrap();
+        match l.trim().split(' ').collect::<Vec<_>>().as_slice() {
+            ["GET", resource, "HTTP/1.1"] => {
+                loop {
+                    let mut l: String = String::new();
+                    rdr.read_line(&mut l).unwrap();
+                    if l.trim().is_empty() { break; }
+                    println!("{l}");
+                }
+                let mut p: std::path::PathBuf = std::path::PathBuf::new();
+                p.push("website");
+                println!("{}",resource.to_owned().to_string());
+                if resource != &"/" { p.push(resource.trim_start_matches("/")); };
+                if resource.contains('?') { p = std::path::PathBuf::from("website".to_string()+(&resource.split('?').collect::<Vec<&str>>()[0].to_owned().to_string())); };
+                if !resource.ends_with('/') && !resource.contains('.') { let n: String = p.to_str().unwrap().to_owned(); p = std::path::PathBuf::from(n+"/index.html"); };
+                if resource.ends_with("/") { p.push("index.html") };
+                println!("{}",p.clone().into_os_string().into_string().unwrap());
+                if Path::new(&p).exists() {
+                    stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+                    stream.write_all(&std::fs::read(p).unwrap()).unwrap();
+                } else {
+                    stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
+                }
+            }
+            _ => todo!()
+        }     
+    }
+}
